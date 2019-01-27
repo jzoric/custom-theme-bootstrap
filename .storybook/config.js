8ff0fd1da8cba61d2ead0269b5a4cf0e4461767f
Jovica Zoric <zoricjovica@gmail.com>
2019-01-27 15:13:47 +0100

    Proposal 1

---
+++ .storybook/config.js
@@ -0,0 +1,26 @@
+import { configure, addDecorator } from '@storybook/html';
+import { withOptions } from '@storybook/addon-options';
+import "../src/sass/my-theme.scss";
+
+addDecorator(withOptions({
+    name: 'Custom theme',
+    url: '#',
+    goFullScreen: false,
+    showStoriesPanel: true,
+    showAddonPanel: true,
+    showSearchBox: false,
+    addonPanelInRight: false,
+    sortStoriesByKind: false,
+    hierarchySeparator: null,
+    hierarchyRootSeparator: null,
+    sidebarAnimations: true,
+    selectedPanel: undefined,
+    enableShortcuts: false,
+}));
+
+const req = require.context('../src/stories', true, /.stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
